0227d0c813867eb96811017b18dd8aa96ad50f39
lavishsaluja <lavishsaluja.ls@gmail.com>
2018-10-03 22:44:40 +0530

    added max flow

---
+++ MaxFlow.cpp
@@ -0,0 +1,20 @@
+//lavishsaluja
+#include<bits/stdc++.h>
+using namespace std;
+#define debug1(x) cout<<x<<"\n"
+#define debug2(x,y) cout<<x<<"-->"<<y<<"\n"
+#define debug3(x,y,z) cout<<x<<"-->"<<y<<"-->"<<z<<"\n"
+
+
+//driving function
+int main(void){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt","w",stdout);
+	#endif
+		
+	return 0;
+}
